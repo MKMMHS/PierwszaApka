7b537ab556ba649c06fc1f16b161d6037303cd42
Michal <mkaniewska3@gmail.com>
2015-05-20 00:14:36 +0200

    nowa mapa

--- app/src/main/java/com/example/micha/pierwszaapka/MapsActivity.java
+++ app/src/main/java/com/example/micha/pierwszaapka/MapsActivity.java
@@ -1,30 +1,31 @@
 package com.example.micha.pierwszaapka;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
 
-import com.example.micha.pierwszaapka.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity{
+
+
     private GoogleMap mMap;
 
-    public void onMapReady(GoogleMap map) {
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(10, 10))
-                .title("Hello world"));
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+
 
     }
 
@@ -32,28 +33,51 @@ public class MapsActivity extends FragmentActivity {
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
-        Uri gmmIntentUri = Uri.parse("geo:51.9715,20.5230");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");
-        if (mapIntent.resolveActivity(getPackageManager()) != null) {
-            startActivity(mapIntent);
-        }
     }
 
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
     private void setUpMapIfNeeded() {
-        if (mMap != null) {
-            return;
-        }
-        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        // Do a null check to confirm that we have not already instantiated the map.
+
         if (mMap == null) {
-            return;
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
         }
-        // Initialize map options. For example:
-        // mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-
-
-
-
-
     }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+
+        mMap.addMarker(new MarkerOptions().position(new LatLng(52.252798, 20.893635)).title("Marcin"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(52.252972,20.895915)).title("Krzysiek"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(52.252249,20.905533)).title("Grzesiek"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(52.254807,20.90182)).title("Marta"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(52.261256,20.894739)).title("Asia"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(52.254893, 20.896005)).title("Ola"));
+    }
+
 }
